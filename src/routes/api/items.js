ff7f6786fe4f3494819430fe97189c7c3523cf91
Ahmad Sadek <ahmadsadek18@hotmail.com>
2026-01-16 17:09:58 +0200

    Remove unused multer-s3 imports

--- src/routes/api/items.js
+++ src/routes/api/items.js
@@ -4,7 +4,6 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const multerS3 = require('multer-s3');
 const { body, validationResult } = require('express-validator');
 const { authenticate, requireUserType } = require('../../middleware/auth');
 const { tenantIsolation, requireOwnership } = require('../../middleware/tenant');
