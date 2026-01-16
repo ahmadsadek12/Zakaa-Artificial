172cc8c2dce632a5f8fc1742387689aea3ad0885
Ahmad Sadek <ahmadsadek18@hotmail.com>
2026-01-16 17:04:30 +0200

    Increase item image upload limit from 5MB to 10MB

--- src/routes/api/items.js
+++ src/routes/api/items.js
@@ -33,7 +33,7 @@ const upload = multer({
     }
   }) : multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024 // 5MB
+    fileSize: 10 * 1024 * 1024 // 10MB
   },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith('image/')) {
